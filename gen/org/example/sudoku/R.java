dd7597557f4645866fb20b3b7f4acad14788d57c
Ben Shyong <likeachild@gmail.com>
2011-03-07 00:51:26 -0500

    added about popup

--- gen/org/example/sudoku/R.java
+++ gen/org/example/sudoku/R.java
@@ -17,15 +17,18 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int menu_about_button=0x7f060002;
-        public static final int menu_continue_button=0x7f060000;
-        public static final int menu_exit_button=0x7f060003;
-        public static final int menu_new_button=0x7f060001;
+        public static final int about_content=0x7f060000;
+        public static final int menu_about_button=0x7f060003;
+        public static final int menu_continue_button=0x7f060001;
+        public static final int menu_exit_button=0x7f060004;
+        public static final int menu_new_button=0x7f060002;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
+        public static final int about_text=0x7f050006;
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
         public static final int menu_about_button=0x7f050004;
